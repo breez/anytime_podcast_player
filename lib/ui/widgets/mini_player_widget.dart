e183a16d073d401f94c05c112bac53b773050a39
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2021-02-02 09:55:25 +0300

    Use buttonColor for play button of Mini Player

--- lib/ui/widgets/mini_player_widget.dart
+++ lib/ui/widgets/mini_player_widget.dart
@@ -170,7 +170,7 @@ class _MiniPlayerBuilderState extends State<_MiniPlayerBuilder> with SingleTicke
                               child: AnimatedIcon(
                                 size: 48.0,
                                 icon: AnimatedIcons.play_pause,
-                                color: Theme.of(context).accentColor,
+                                color: Theme.of(context).buttonColor,
                                 progress: _playPauseController,
                               ),
                             );
